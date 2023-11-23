44c3e40884607b760bea4129b9faae5e611506a1
Silvision <richardmnguyen@hotmail.com>
2023-11-22 20:52:30 -0600

    added outline and outline offset

--- src/components/StyledComponents/ProjectSquare.tsx
+++ src/components/StyledComponents/ProjectSquare.tsx
@@ -6,12 +6,12 @@ import { Scale } from "@mui/icons-material"
 const bounce = keyframes`
   25% {
     transform: scale(1.05);
-    box-shadow: 0 0 35px #1e9bff;
+    box-shadow: 0 0 60px #1e9bff;
   }
 
   100% {
     transform: scale(1);
-    box-shadow: 0 0 10px #1e9bff;
+    box-shadow: 0 0 30px #1e9bff;
   }
 `
 
@@ -23,11 +23,13 @@ function ProjectSquare() {
                 variant="contained"   
                 sx={{
                     letterSpacing: '0.1em',      
-                    padding: '50px 50px',
+                    padding: '5em 5em',
                     transition: '0.5s', 
                     '&:hover': {
                         background: '#1e9bff',
                         animation: `${bounce} 0.5s ease-in-out forwards`,
+                        outline: 'solid 0.3em rgba(138, 150, 152, 0.8)',
+                        outlineOffset: '0.25em',
                     },
                 }}
             >
