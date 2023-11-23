39927ccd362478b06dec49d1cc0b0e2c19baa617
Silvision <richardmnguyen@hotmail.com>
2023-11-22 20:38:12 -0600

    edited tsconfig to work with css props, and also got animation working on button

--- src/components/StyledComponents/ProjectSquare.tsx
+++ src/components/StyledComponents/ProjectSquare.tsx
@@ -1,22 +1,37 @@
 import { Button } from "@mui/material"
-import { red } from "@mui/material/colors"
+import { keyframes } from "@emotion/react"
+import { Scale } from "@mui/icons-material"
 
 
+const bounce = keyframes`
+  25% {
+    transform: scale(1.05);
+    box-shadow: 0 0 35px #1e9bff;
+  }
 
-
+  100% {
+    transform: scale(1);
+    box-shadow: 0 0 10px #1e9bff;
+  }
+`
 
 function ProjectSquare() {
 
     return (
         <>
             <Button
-                variant="contained"            
+                variant="contained"   
                 sx={{
-                    boxShadow: 5
+                    letterSpacing: '0.1em',      
+                    padding: '50px 50px',
+                    transition: '0.5s', 
+                    '&:hover': {
+                        background: '#1e9bff',
+                        animation: `${bounce} 0.5s ease-in-out forwards`,
+                    },
                 }}
             >
                 PROJECT 1
-
             </Button>
         </>
     )
