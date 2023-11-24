5d5f3e1b0204fea9e918563449e090cae4d95ec0
Silvision <richardmnguyen@hotmail.com>
2023-11-24 13:27:21 -0600

    reformatted with pretteir

--- src/components/StyledComponents/StyledButton.tsx
+++ src/components/StyledComponents/StyledButton.tsx
@@ -1,61 +1,59 @@
-import { ThemeProvider } from '@emotion/react';
-import Button from '@mui/material/Button'
-import Icon from '@mui/material/Icon';
-import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from "@emotion/react";
+import Button from "@mui/material/Button";
+import Icon from "@mui/material/Icon";
+import { createTheme } from "@mui/material/styles";
 
 type StyledButtonProps = {
-    icon: string;
+  icon: string;
+};
+
+declare module "@mui/material/styles" {
+  interface Palette {
+    darkButtons: Palette["primary"];
+  }
+
+  interface PaletteOptions {
+    darkButtons?: PaletteOptions["primary"];
+  }
 }
 
-declare module '@mui/material/styles' {
-    interface Palette {
-      darkButtons: Palette['primary'];
-    }
-  
-    interface PaletteOptions {
-      darkButtons?: PaletteOptions['primary'];
-    }
-}
-  
-  // Update the Button's color options to include an ochre option
-declare module '@mui/material/Button' {
-    interface ButtonPropsColorOverrides {
-      darkButtons: true;
-    }
+// Update the Button's color options to include an ochre option
+declare module "@mui/material/Button" {
+  interface ButtonPropsColorOverrides {
+    darkButtons: true;
+  }
 }
 
 const theme = createTheme({
-    palette: {
-      darkButtons: {
-        main: '#272727',
-        light: '#f0f0f0',
-        dark: '#2d2d2d',
-        contrastText: '#a5a5a5',
-      },
+  palette: {
+    darkButtons: {
+      main: "#272727",
+      light: "#f0f0f0",
+      dark: "#2d2d2d",
+      contrastText: "#a5a5a5",
     },
+  },
 });
 
-function StyledButton({icon}: StyledButtonProps) { 
-
-    return (
-        <ThemeProvider theme={theme}>
-            <Button
-                variant="contained"
-                color="darkButtons"
-                sx={{
-                    borderRadius: '0.5em',
-                    '&:hover': {
-                    '& .MuiIcon-root': {
-                        color: '#f0f0f0',
-                    },
-                    },
-                }}
-            >
-                <Icon sx={{ color: 'inherit'}}>{icon}</Icon>
-            </Button>
-        </ThemeProvider>
-    );
-
+function StyledButton({ icon }: StyledButtonProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Button
+        variant="contained"
+        color="darkButtons"
+        sx={{
+          borderRadius: "0.5em",
+          "&:hover": {
+            "& .MuiIcon-root": {
+              color: "#f0f0f0",
+            },
+          },
+        }}
+      >
+        <Icon sx={{ color: "inherit" }}>{icon}</Icon>
+      </Button>
+    </ThemeProvider>
+  );
 }
 
-export default StyledButton
+export default StyledButton;
