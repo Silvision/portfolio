16b077f5d573930f3cc359f9eafcc80352575713
Silvision <richardmnguyen@hotmail.com>
2023-12-02 23:48:58 -0600

    added responsive grid

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -2,7 +2,7 @@ import ProjectSquare from "./components/StyledComponents/ProjectSquare";
 import image1 from "../src/assets/cover15.jpg";
 import video1 from "../src/assets/EcosphereVideo.mp4";
 import { motion } from "framer-motion";
-import { Grid } from "@mui/material";
+import "./Projects.css";
 
 function Projects() {
   return (
@@ -11,14 +11,23 @@ function Projects() {
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
     >
-      <Grid>
-        <ProjectSquare
-          linkURL="ecosphere"
-          imageURL={image1}
-          videoURL={video1}
-        />
-        <ProjectSquare linkURL="ecosphere" imageURL={""} videoURL={""} />
-      </Grid>
+      <div className="container">
+        <div className="item">
+          <ProjectSquare
+            linkURL="ecosphere"
+            imageURL={image1}
+            videoURL={video1}
+          />
+        </div>
+
+        <div className="item">
+          <ProjectSquare linkURL="ecosphere" imageURL={""} videoURL={""} />
+        </div>
+
+        <div className="item">
+          <ProjectSquare linkURL="ecosphere" imageURL={""} videoURL={""} />
+        </div>
+      </div>
     </motion.div>
   );
 }
