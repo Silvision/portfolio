ca5099c5da0eb4b19735f5cd56f5212d56edae12
Silvision <richardmnguyen@hotmail.com>
2024-01-18 21:15:57 -0600

    got a prev and next button going ig

--- src/Ecosphere.tsx
+++ src/Ecosphere.tsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
 import { Swiper, SwiperSlide, SwiperClass } from 'swiper/react';
-import { Thumbs, Scrollbar } from 'swiper/modules';
+import { Thumbs, Scrollbar, Navigation } from 'swiper/modules';
 import { ecosphereMedia } from "./assets/images";
 import "./Ecosphere.css";
 
 import 'swiper/css';
 import 'swiper/css/scrollbar'
+import 'swiper/css/navigation'
 
 
 function Ecosphere() {
@@ -34,25 +35,38 @@ function Ecosphere() {
             <SwiperSlide> <img src={ecosphereMedia[0]}/> </SwiperSlide>
             <SwiperSlide> Slide 2 </SwiperSlide>
             <SwiperSlide> Slide 3 </SwiperSlide>
+            <SwiperSlide> Slide 4 </SwiperSlide>
+            <SwiperSlide> Slide 5 </SwiperSlide>
+            <SwiperSlide> Slide 6 </SwiperSlide>
+            <SwiperSlide> Slide 7 </SwiperSlide>
 
           </Swiper>
 
           {/*Thumbs Swiper*/}
           <Swiper
-            modules={[Thumbs, Scrollbar]}
+            modules={[Thumbs, Scrollbar, Navigation]}
+            navigation={{prevEl: '.swiperThumbs-prevEl', nextEl: '.swiperThumbs-nextEl', }}
             scrollbar={{el: '.swiperThumbs-scrollbar', draggable: true, hide: false}}
             watchSlidesProgress
-            slidesPerView={1}
+            slidesPerView={3}
             onSwiper={setThumbsSwiper}
             className="swiperThumbs"
           > 
             <SwiperSlide> <img src={ecosphereMedia[0]}/> </SwiperSlide>
             <SwiperSlide> Slide 2 </SwiperSlide>
             <SwiperSlide> Slide 3 </SwiperSlide>
+            <SwiperSlide> Slide 4 </SwiperSlide>
+            <SwiperSlide> Slide 5 </SwiperSlide>
+            <SwiperSlide> Slide 6 </SwiperSlide>
+            <SwiperSlide> Slide 7 </SwiperSlide>
 
           </Swiper>
 
-          <div className="swiperThumbs-scrollbar"></div>
+          <div className="swiperThumbs-navigation">
+            <div className="swiperThumbs-prevEl">Prev</div>
+            <div className="swiperThumbs-scrollbar"></div>
+            <div className="swiperThumbs-nextEl">Next</div>
+          </div>
 
         </div>
       </div>
