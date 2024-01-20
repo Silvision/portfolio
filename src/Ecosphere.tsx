1bf2c2476e6237647ef536227cd1f0f0c56d8531
Silvision <richardmnguyen@hotmail.com>
2024-01-20 00:49:39 -0600

    removed blue background, added text

--- src/Ecosphere.tsx
+++ src/Ecosphere.tsx
@@ -13,7 +13,20 @@ function Ecosphere() {
       exit={{ opacity: 0 }}
     >
       <div className="container-project">
-        <div className="container-project-description"> EcoSphere </div>
+        <div className="container-project-description"> 
+          <h1> EcoSphere </h1> 
+          <div>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+              Pellentesque non consequat diam, accumsan gravida felis. Nam eu massa a
+              ccumsan mauris convallis vulputate. Sed a enim sit amet ligula bibendum
+               luctus eu vitae tellus. Sed ante ligula, eleifend sit amet finibus eget, 
+               efficitur in sem. Morbi auctor tincidunt quam, in dapibus quam ornare vitae. Se
+               d ut malesuada nisl. Nunc interdum dignissim tempor. Proin ipsum ante, congue eu qua
+               m at, ornare dictum velit. Vestibulum egestas erat eget enim ullamcorper, sollicitudin p
+               ellentesque leo congue. Praesent rhoncus sed arcu sed pharetra. </p>
+          </div>
+        
+        </div>
         <div className="container-project-slider"> <ProjectSwiper projectMedia={ecosphereMedia}/> </div>
         
       </div>
