ae6b91a1d5722455e879f2fac5768ee7b8e9b4af
Silvision <richardmnguyen@hotmail.com>
2024-01-22 14:43:50 -0600

    renamed files, simplified image importing process

--- src/assets/images.tsx
+++ src/assets/images.tsx
@@ -1,18 +1,13 @@
-import ecosphereCover from './Ecosphere/ecosphereCover.png'
-import ecosphereVideo1 from './Ecosphere/EcosphereVideo.mp4'
-import ecosphereImage1 from './Ecosphere/ecosphereImg1.png'
-import ecosphereImage2 from './Ecosphere/ecosphereImg2.png'
-import ecosphereImage3 from './Ecosphere/ecosphereImg3.png'
-import ecosphereImage4 from './Ecosphere/ecosphereImg4.png'
-import ecosphereImage5 from './Ecosphere/ecosphereImg5.png'
-import ecosphereImage6 from './Ecosphere/ecosphereImg6.png'
-import ecosphereImage7 from './Ecosphere/ecosphereImg7.png'
-import ecosphereImage8 from './Ecosphere/ecosphereImg8.png'
-import ecosphereImage9 from './Ecosphere/ecosphereImg9.png'
-import ecosphereImage10 from './Ecosphere/ecosphereImg10.png'
-import ecosphereImage11 from './Ecosphere/ecosphereImg11.png'
+const importAll = (importedObject: Record<string, string>) => {
+    const media: string[] = [];
+    const values = Object.values(importedObject);
+    for (const value of values) {
+        media.push(value);
+    }
+    return media;
+}
 
+const ecosphereMediaImport = importAll(import.meta.glob('./Ecosphere/*.{png,mp4}', {as: 'url', eager: true}));
 
-export const projectsMedia: string[] = [ecosphereCover, ecosphereVideo1]
-export const ecosphereMedia: string[] = [ecosphereCover, ecosphereImage1, ecosphereImage2, ecosphereImage3, ecosphereImage4, ecosphereImage5, ecosphereImage6, ecosphereImage7, ecosphereImage8, ecosphereImage9, ecosphereImage10, ecosphereImage11]
- 
+export const projectsMedia: string[] = [ecosphereMediaImport[0], ecosphereMediaImport[1]]
+export const ecosphereMedia: string[] = ecosphereMediaImport.slice(2);
