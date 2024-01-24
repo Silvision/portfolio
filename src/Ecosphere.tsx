c5402e1915f3b9cfa0a93c74161aa811017e4161
Silvision <richardmnguyen@hotmail.com>
2024-01-24 12:26:32 -0600

    added some more styling to description container, and added ecosphere description

--- src/Ecosphere.tsx
+++ src/Ecosphere.tsx
@@ -14,16 +14,22 @@ function Ecosphere() {
         <div className="container-project-description"> 
           <h1> EcoSphere </h1> 
           <div>
-            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-              Pellentesque non consequat diam, accumsan gravida felis. Nam eu massa a
-              ccumsan mauris convallis vulputate. Sed a enim sit amet ligula bibendum
-               luctus eu vitae tellus. Sed ante ligula, eleifend sit amet finibus eget, 
-               efficitur in sem. Morbi auctor tincidunt quam, in dapibus quam ornare vitae. Se
-               d ut malesuada nisl. Nunc interdum dignissim tempor. Proin ipsum ante, congue eu qua
-               m at, ornare dictum velit. Vestibulum egestas erat eget enim ullamcorper, sollicitudin p
-               ellentesque leo congue. Praesent rhoncus sed arcu sed pharetra. </p>
+            <p>
+              EcoSphere is a 3D roguelike adventure game.
+              <br/><br/>
+              Playing as either a Mage or Sword class, the player adventures through multiple biomes and encounters different types of enemies to destroy for EXP, gaining
+              new strengths and abilities.
+              Each run can have randomized abilities based on an element unlocked through the biomes the player is free to choose to adventure through.
+              The randomization allows for new methods of fighting through each playthrough.
+              <br/><br/>
+              Some featured abilities can range from heals, massive sword slashes, meteor crashes, fireworks, and more.
+              As for enemies, there are plenty to fight and discover, from fire types and stone golem bosses protecting their beloved, to massive penguins and a 
+              cave teethering with ice golems.
+            </p>
           </div>
-        
+
+          <button> Test </button>
+
         </div>
         <div className="container-project-slider"> 
           <ProjectSwiper projectMedia={ecosphereMedia} embededVideo={true} videoLink="https://www.youtube.com/embed/jVD6l2VARas?si=w3vGxLzcWUKnrOfv"/> 
