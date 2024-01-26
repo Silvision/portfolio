6c8c54b98c0126677e2a6d3f68c81aabb98c222b
Silvision <richardmnguyen@hotmail.com>
2024-01-25 18:49:09 -0600

    made condition to check logo or svg, and scale the icon

--- src/components/StyledComponents/StyledButton.tsx
+++ src/components/StyledComponents/StyledButton.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from "@emotion/react";
 import Button from "@mui/material/Button";
 import Icon from "@mui/material/Icon";
 import { createTheme } from "@mui/material/styles";
+import React, { ReactElement, useState } from "react";
 
 type StyledButtonProps = {
   icon: React.ReactNode;
@@ -62,7 +63,19 @@ const theme = createTheme({
 });
 
 function StyledButton({ icon, buttonColor, buttonSize}: StyledButtonProps) {
-  const IconComponent = typeof icon === 'string' ? <Icon>{icon}</Icon> : icon;
+  let iconComponent;
+  // Check if it's a string for normal icons
+  if (typeof icon === 'string'){
+    iconComponent = <Icon style={{scale: buttonSize === 'extraLarge' ? "300%" : "100%"}}>{icon}</Icon>
+  }
+  // Check if it's something else such as an ReactElement, where we passed in a component (example passed in <GitHubIcon />)
+  else {
+    iconComponent = (React.cloneElement(icon as ReactElement, {
+      style: {
+        transform: `scale(${buttonSize === 'extraLarge' ? 3 : 1})`,
+      }}
+    ))
+  }
 
   return (
     <ThemeProvider theme={theme}>
@@ -79,7 +92,7 @@ function StyledButton({ icon, buttonColor, buttonSize}: StyledButtonProps) {
           },
         }}
       >
-        {IconComponent}
+        {iconComponent}
       </Button>
     </ThemeProvider>
   );
